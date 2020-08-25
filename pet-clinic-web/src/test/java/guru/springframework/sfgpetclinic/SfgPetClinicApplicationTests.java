348f2b6213a7eb6c03949b4e54ba524ef7663338
Elnur Aliyev <aelnur@guavapay.com>
2020-08-26 00:02:05 +0400

    update pom.xml to include SNAPSHOT

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,9 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
